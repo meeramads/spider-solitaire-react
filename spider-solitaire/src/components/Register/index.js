32802b8d29d38120254b2319908c79a7716a4963
Meera <meera.madhavan.95@gmail.com>
2019-09-07 21:01:31 -0700

    set up register functionality

--- spider-solitaire/src/components/Register/index.js
+++ spider-solitaire/src/components/Register/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
 
+import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 
 const RegisterPage = () => (
     <div>
         <h1>SignUp</h1>
+        <RegisterForm />
     </div>
 );
 
@@ -14,24 +17,37 @@ const INITIAL_STATE = {
     email: '',
     passwordOne: '',
     passwordTwo: '',
+    highScore: 0,
     error: null
 }
 
-class RegisterForm extends Component {
+class RegisterFormBase extends Component {
     constructor(props){
         super(props);
 
         this.state = { ...INITIAL_STATE };
     }
 
+    const isInvalid =
+        passwordOne !== passwordTwo ||
+        passwordOne === '' ||
+        username === '';
+
+
     handleSubmit = (e) => {
+        e.preventDefault();
+
         const { username, email, passwordOne } = this.state;
 
         this.props.firebase
             .doCreateUserWithEmailAndPassword(email,passwordOne)
             .then(authUser => { 
-                this.setState({ ...INITIAL_STATE })
+                this.setState({ ...INITIAL_STATE });
+                this.props.history.push(ROUTES.LANDING);
             })
+            .catch(error => {
+                this.setState({ error });
+            });
 
     }
 
@@ -70,7 +86,7 @@ class RegisterForm extends Component {
                     type="password"
                     placeholder="Confirm Password"
                 />
-                <button type="submit">Sign Up</button>
+                <button disabled={this.isInvalid} type="submit">Sign Up</button>
 
                 {error && <p>{error.message}</p>}
             </form>
@@ -83,6 +99,11 @@ const RegisterLink = () => (
         Don't have an account? <Link to={ROUTES.REGISTER}>Register</Link>
     </p>
 );
+
+const RegisterForm = compose(
+    withRouter, 
+    withFirebase
+  )(RegisterFormBase);
 
 export default RegisterPage;
 
